1f18ae19296232cd3e90196d992a8915c41fdde4
mike-work <michael.hauser-raspe@cantab.net>
2014-05-20 02:28:59 +0100

    added whitespace fix to stop tabs being highlighted
    removing go-run

--- prelude-go.el
+++ prelude-go.el
@@ -120,6 +120,9 @@ ID is used to look-up the pair close.  ACTION and CONTEXT are ignored."
        ;; gofmt on save
        (add-hook 'before-save-hook 'gofmt-before-save nil t)
 
+       ;; stop whitespace being highlighted
+       (whitespace-toggle-options '(tabs))
+
        ;; Company mode settings
        (set (make-local-variable 'company-backends) '(company-go))
 
